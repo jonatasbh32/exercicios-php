5ac4f222cebc46de3dcc798a6f95f0062586be39
jonatas dias <jonatasbh32@yahoo.com.br>
2015-05-14 11:34:57 -0300

    exercicios 1 e 2

---
+++ p1_exercicio1/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 3+4*5;
+$b = 1+12/((7+2)/3)+(6-2);
+$c = sqrt (9)+ sqrt(16);
+$d = 8/4+2*3;
+$e = 3+16/2+5;
+$f = 21 / 4 / 2;
+$g = 2*(10-3*3)-1;
+$h =  24 / 4-2;
+$i = 5*(3+(2+3))/2+1;
+$J = 11 % 4+9 / 3;
+
+echo "o resultado é ".$a."<br/>";
+echo "o resultado é ".$b."<br/>";
+echo "o resultado é ".$c."<br/>";
+echo "o resultado é ".$d."<br/>";
+echo "o resultado é ".$e."<br/>";
+echo "o resultado é ".$f."<br/>";
+echo "o resultado é ".$g."<br/>";
+echo "o resultado é ".$h."<br/>";
+echo "o resultado é ".$i."<br/>";
+echo "o resultado é ".$J."<br/>";
+?>
