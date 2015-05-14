582dd35ee7a3bed2335648c9fa498b90b6405392
jonatas dias <jonatasbh32@yahoo.com.br>
2015-05-14 13:40:55 -0300

    exercicio 2

--- p1_exercicio2/index.php
+++ p1_exercicio2/index.php
@@ -1,6 +1,51 @@
 <?php
 
+$resultado1 = 3+2;
+$a = 6 * $resultado1; 
+
+$resultado1 = 3 + 20 -2;
+$resultado2 = 2 * (9-6);
+
+$b = $resultado1 / $resultado2;
+
+$resultado1 = 10*8+3;
+$c  = $resultado1 * 9;
+
+$resultado1 = (3+2)*6;
+
+$d = 2 + $resultado1;
+
+$resultado1 = 6/3;
+$resultado2 = 8/2;
+
+$e = $resultado1 + $resultado2;
+
+$resultado1 = (2+3)*6;
+$resultado2 = 2+4;
+
+$f = $resultado1 / $resultado2;
+
+$resultado1 = 3 + (8/2) * 4;
+$resultado2 = 3*2;
+
+$g = $resultado1 + $resultado2;
+
+$resultado1 = 8/(3+1);
+$h = 2 * $resultado1;
+
+$resultado1 = (6*3*3) + 6;
+
+$j = $resultado1 -10;
 
 
+echo "O que resultado é = ".$a."<br/>";
+echo "O que resultado é = ".$b."<br/>";
+echo "O que resultado é = ".$c."<br/>";
+echo "O que resultado é = ".$d."<br/>";
+echo "O que resultado é = ".$e."<br/>";
+echo "O que resultado é = ".$f."<br/>";
+echo "O que resultado é = ".$g."<br/>";
+echo "O que resultado é = ".$h."<br/>";
+echo "O que resultado é = ".$j."<br/>";
 ?>
 
